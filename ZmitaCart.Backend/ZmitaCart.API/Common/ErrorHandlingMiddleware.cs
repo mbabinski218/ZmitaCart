74ac6157400196ca686adff49cbe3332b1d74995
matebab551 <matebab551@student.polsl.pl>
2023-04-13 00:44:46 +0200

    Error handling middleware

---
+++ ZmitaCart.Backend/ZmitaCart.API/Common/ErrorHandlingMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using FluentValidation;
+
+namespace ZmitaCart.API.Common;
+
+public class ErrorHandlingMiddleware : IMiddleware
+{
+	private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+	public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+	{
+		_logger = logger;
+	}
+
+	public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+	{
+		try
+		{
+			await next.Invoke(context);
+		}
+		catch (NotImplementedException)
+		{
+			context.Response.StatusCode = StatusCodes.Status501NotImplemented;
+			context.Response.ContentType = "text/plain";
+			await context.Response.WriteAsync("Not implemented yet.");
+		}
+		catch (ValidationException ex)
+		{
+			context.Response.StatusCode = StatusCodes.Status400BadRequest;
+			context.Response.ContentType = "application/json";
+			await context.Response.WriteAsync(JsonSerializer
+				.Serialize(ex.Errors.Select(vf => vf.ErrorMessage)));
+		}
+		catch (Exception ex)
+		{
+			_logger.LogCritical("{msg}", ex.Message);
+			context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+			context.Response.ContentType = "text/plain";
+			await context.Response.WriteAsync("An unexpected error occurred.");
+		}
+	}
+}
--- ZmitaCart.Backend/ZmitaCart.API/Program.cs
+++ ZmitaCart.Backend/ZmitaCart.API/Program.cs
@@ -1,12 +1,13 @@
 using ZmitaCart.API.Hubs;
+using ZmitaCart.API.Common;
 using ZmitaCart.Application;
 using ZmitaCart.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
 builder.Services.AddSignalR();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
@@ -19,6 +20,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
 app.MapControllers();
 app.UseRouting();

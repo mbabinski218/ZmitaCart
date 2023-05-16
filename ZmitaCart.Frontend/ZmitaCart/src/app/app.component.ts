131948bde2891ef6ca960175ebddbeae7cebf4b1
PanJan44 <jasjanek33@gmail.com>
2023-05-16 23:38:47 +0200

    small fixes

--- ZmitaCart.Frontend/ZmitaCart/src/app/app.component.ts
+++ ZmitaCart.Frontend/ZmitaCart/src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
+
+export const customIcons: [string, string][] = [
+  ["my-camera", "camera-add.svg"],];
 
 @Component({
-    selector: 'pp-app-root',
-    standalone: true,
-    imports: [CommonModule, RouterModule],
-    templateUrl: './app.component.html',
+  selector: 'pp-app-root',
+  standalone: true,
+  imports: [CommonModule, RouterModule, HttpClientModule],
+  templateUrl: './app.component.html',
 })
-export class AppComponent {}
+
+export class AppComponent {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    customIcons.forEach(([iconName, icon]) => {
+      iconRegistry.addSvgIcon(
+        iconName,
+        sanitizer.bypassSecurityTrustResourceUrl(`../assets/images/${icon}`),
+      );
+    });
+  }
+}
--- ZmitaCart.Frontend/ZmitaCart/src/app/modules/add-offer/add-offer.component.ts
+++ ZmitaCart.Frontend/ZmitaCart/src/app/modules/add-offer/add-offer.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'pp-add-offer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './add-offer.component.html',
   styleUrls: ['./add-offer.component.scss']
 })

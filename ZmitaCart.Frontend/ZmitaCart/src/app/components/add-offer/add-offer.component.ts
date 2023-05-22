bcc04100060129a0887d0ca46d9a5123fa990fbe
Filip <filipmiera1@gmail.com>
2023-05-22 21:52:01 +0200

    fix?

--- ZmitaCart.Frontend/ZmitaCart/src/app/components/add-offer/add-offer.component.ts
+++ ZmitaCart.Frontend/ZmitaCart/src/app/components/add-offer/add-offer.component.ts
@@ -1,14 +1,16 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'pp-add-offer',
   standalone: true,
   imports: [CommonModule, MatIconModule, MatButtonModule],
   templateUrl: './add-offer.component.html',
-  styleUrls: ['./add-offer.component.scss']
+  styleUrls: ['./add-offer.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AddOfferComponent {
 

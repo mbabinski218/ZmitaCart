87ea6e79ff34df7a0cd08b1be1f08c73e064d074
PanJan44 <jasjanek33@gmail.com>
2023-05-22 21:04:21 +0200

    dodawanie zdjec podstawka

--- ZmitaCart.Frontend/ZmitaCart/src/app/modules/add-offer/add-offer.component.ts
+++ ZmitaCart.Frontend/ZmitaCart/src/app/modules/add-offer/add-offer.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'pp-add-offer',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule],
   templateUrl: './add-offer.component.html',
   styleUrls: ['./add-offer.component.scss']
 })

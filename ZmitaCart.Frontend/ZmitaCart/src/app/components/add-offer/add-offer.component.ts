8894b90fafe167d0ab50a56f728c05b5f886d004
PanJan44 <jasjanek33@gmail.com>
2023-05-29 16:15:20 +0200

    Przed komponentem kategorii

--- ZmitaCart.Frontend/ZmitaCart/src/app/components/add-offer/add-offer.component.ts
+++ ZmitaCart.Frontend/ZmitaCart/src/app/components/add-offer/add-offer.component.ts
@@ -1,17 +1,38 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatIconModule } from "@angular/material/icon";
-import { MatButtonModule } from "@angular/material/button";
+import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
 
 @Component({
   selector: 'pp-add-offer',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './add-offer.component.html',
   styleUrls: ['./add-offer.component.scss'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AddOfferComponent {
+export class AddOfferComponent implements OnInit {
+  createOffer: FormGroup;
+  inputTitle: string;
+  characterCount: number;
+  // constructor(private fb: FormGroup
+  // ) {
+  //
+  // }
+
+  ngOnInit(): void {
+    this.createOffer = new FormGroup({
+      title: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+      description: new FormControl(null as string, [])
+    })
+    this.createOffer.get('title').valueChanges.subscribe(res => {
+      this.characterCount = res.length;
+    });
+
+  }
 
 }
+

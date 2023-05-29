07eb472eff5efd8fca6d66f4593d77a5c2de364e
mbabinski218 <matebab551@student.polsl.pl>
2023-05-29 18:42:57 +0200

    fix

--- ZmitaCart.Backend/ZmitaCart.Infrastructure/Persistence/DatabaseSeeder.cs
+++ ZmitaCart.Backend/ZmitaCart.Infrastructure/Persistence/DatabaseSeeder.cs
@@ -9,53 +9,51 @@ namespace ZmitaCart.Infrastructure.Persistence;
 
 public class DatabaseSeeder : IDatabaseSeeder
 {
-    private readonly ApplicationDbContext _dbContext;
-    private readonly RoleManager<IdentityRole<int>> _roleManager;
+	private readonly ApplicationDbContext _dbContext;
+	private readonly RoleManager<IdentityRole<int>> _roleManager;
 
-    public DatabaseSeeder(ApplicationDbContext dbContext, RoleManager<IdentityRole<int>> roleManager)
-    {
-        _dbContext = dbContext;
-        _roleManager = roleManager;
-    }
+	public DatabaseSeeder(ApplicationDbContext dbContext, RoleManager<IdentityRole<int>> roleManager)
+	{
+		_dbContext = dbContext;
+		_roleManager = roleManager;
+	}
 
-    public async Task Seed()
-    {
-        if (!await _dbContext.Database.CanConnectAsync())
-        {
-            return;
-        }
+	public async Task Seed()
+	{
+		if (!await _dbContext.Database.CanConnectAsync())
+		{
+			return;
+		}
 
-        if (!Equals(_dbContext.Roles.Select(r => r.Name), Role.SupportedRoles))
-        {
-            await SeedRoles();
-        }
+		await SeedRoles();
 
-        if (!await _dbContext.Categories.AnyAsync())
-        {
-            await SeedData();
-            await _dbContext.SaveChangesAsync();
-        }
-    }
+		if (!await _dbContext.Categories.AnyAsync())
+		{
+			await SeedData();
+		}
 
-    private async Task SeedRoles()
-    {
-        foreach (var supportedRole in Role.SupportedRoles)
-        {
-            var role = new IdentityRole<int>(supportedRole);
+		await _dbContext.SaveChangesAsync();
+	}
 
-            if (!_dbContext.Roles.Contains(role))
-            {
-                await _roleManager.CreateAsync(role);
-            }
-        }
-    }
+	private async Task SeedRoles()
+	{
+		foreach (var supportedRole in Role.SupportedRoles)
+		{
+			var role = new IdentityRole<int>(supportedRole);
 
-    private async Task SeedData()
-    {
-        var basePath = Directory.GetCurrentDirectory();
-        var pathToFile = Path.Combine(basePath, @"..\\ZmitaCart.Infrastructure\Persistence\Samples.txt");
-        var script = await File.ReadAllTextAsync(pathToFile);
+			if (!_dbContext.Roles.Contains(role))
+			{
+				await _roleManager.CreateAsync(role);
+			}
+		}
+	}
 
-        await _dbContext.Database.ExecuteSqlRawAsync(script);
-    }
+	private async Task SeedData()
+	{
+		var basePath = Directory.GetCurrentDirectory();
+		var pathToFile = Path.Combine(basePath, @"..\\ZmitaCart.Infrastructure\Persistence\Samples.txt");
+		var script = await File.ReadAllTextAsync(pathToFile);
+
+		await _dbContext.Database.ExecuteSqlRawAsync(script);
+	}
 }

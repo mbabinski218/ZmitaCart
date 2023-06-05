90703eaa402d09a8d0de1aed4078222ffc7a5b77
mbabinski218 <matebab551@student.polsl.pl>
2023-06-05 22:01:37 +0200

    Poprawny error i oferty usera

---
+++ ZmitaCart.Backend/ZmitaCart.Application/Queries/OfferQueries/GetUserOffers/GetUserOffersHandler.cs
@@ -0,0 +1,34 @@
+﻿using FluentResults;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using ZmitaCart.Application.Common;
+using ZmitaCart.Application.Common.Errors;
+using ZmitaCart.Application.Dtos.OfferDtos;
+using ZmitaCart.Application.Interfaces;
+using ZmitaCart.Application.Services;
+
+namespace ZmitaCart.Application.Queries.OfferQueries.GetUserOffers;
+
+public class GetUserOffersHandler : IRequestHandler<GetUserOffersQuery, Result<PaginatedList<OfferInfoDto>>>
+{
+	private readonly IOfferRepository _offerRepository;
+	private readonly ICurrentUserService _currentUserService;
+
+	public GetUserOffersHandler(IOfferRepository offerRepository, ICurrentUserService currentUserService)
+	{
+		_offerRepository = offerRepository;
+		_currentUserService = currentUserService;
+	}
+
+	public async Task<Result<PaginatedList<OfferInfoDto>>> Handle(GetUserOffersQuery request, CancellationToken cancellationToken)
+	{
+		if (_currentUserService.UserId is null)
+		{
+			return Result.Fail(new UnauthorizedError("You must be logged in to access this resource"));
+		}
+
+		var userId = int.Parse(_currentUserService.UserId);
+		
+		return await _offerRepository.GetUserOffersAsync(userId, request.PageNumber, request.PageSize);
+	}
+}

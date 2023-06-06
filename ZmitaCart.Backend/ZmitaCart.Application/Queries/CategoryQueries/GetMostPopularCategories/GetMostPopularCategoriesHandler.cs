9a22c6f852a9cc9182379631aaa346272ad795a6
mbabinski218 <matebab551@student.polsl.pl>
2023-06-06 23:20:58 +0200

    najpopularniejsze kategorie

---
+++ ZmitaCart.Backend/ZmitaCart.Application/Queries/CategoryQueries/GetMostPopularCategories/GetMostPopularCategoriesHandler.cs
@@ -0,0 +1,20 @@
+﻿using FluentResults;
+using MediatR;
+using ZmitaCart.Application.Interfaces;
+
+namespace ZmitaCart.Application.Queries.CategoryQueries.GetMostPopularCategories;
+
+public class GetMostPopularCategoriesHandler : IRequestHandler<GetMostPopularCategoriesQuery, Result<List<string>>>
+{
+	private readonly ICategoryRepository _categoryRepository;
+
+	public GetMostPopularCategoriesHandler(ICategoryRepository categoryRepository)
+	{
+		_categoryRepository = categoryRepository;
+	}
+
+	public async Task<Result<List<string>>> Handle(GetMostPopularCategoriesQuery request, CancellationToken cancellationToken)
+	{
+		return await _categoryRepository.GetMostPopularCategoriesAsync(request.NumberOfCategories);
+	}
+}
---
+++ ZmitaCart.Backend/ZmitaCart.Application/Queries/CategoryQueries/GetMostPopularCategories/GetMostPopularCategoriesQuery.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace ZmitaCart.Application.Queries.CategoryQueries.GetMostPopularCategories;
+
+public record GetMostPopularCategoriesQuery(int NumberOfCategories) : IRequest<Result<List<string>>>;

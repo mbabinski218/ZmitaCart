0af328dbcaca8a40d534d34a772e9a31d180fafa
mbabinski218 <matebab551@student.polsl.pl>
2023-06-07 23:05:30 +0200

    Kondycja po polsku i ulubione w GetOffersByCategoriesName

--- ZmitaCart.Backend/ZmitaCart.Application/Queries/OfferQueries/GetOffersByCategoriesName/GetOffersByCategoriesNameHandler.cs
+++ ZmitaCart.Backend/ZmitaCart.Application/Queries/OfferQueries/GetOffersByCategoriesName/GetOffersByCategoriesNameHandler.cs
@@ -3,21 +3,51 @@ using MediatR;
 using ZmitaCart.Application.Common;
 using ZmitaCart.Application.Dtos.OfferDtos;
 using ZmitaCart.Application.Interfaces;
+using ZmitaCart.Application.Services;
 
 namespace ZmitaCart.Application.Queries.OfferQueries.GetOffersByCategoriesName;
 
-public class GetOffersByCategoriesNameHandler : IRequestHandler<GetOffersByCategoriesNameQuery, Result<List<NamedList<string, OfferInfoDto>>>>
+public class GetOffersByCategoriesNameHandler : IRequestHandler<GetOffersByCategoriesNameQuery,
+	Result<List<NamedList<string, OfferInfoDto>>>>
 {
 	private readonly IOfferRepository _offerRepository;
+	private readonly ICurrentUserService _currentUserService;
 
-	public GetOffersByCategoriesNameHandler(IOfferRepository offerRepository)
+	public GetOffersByCategoriesNameHandler(IOfferRepository offerRepository, ICurrentUserService currentUserService)
 	{
 		_offerRepository = offerRepository;
+		_currentUserService = currentUserService;
 	}
 
-	public async Task<Result<List<NamedList<string, OfferInfoDto>>>> Handle(GetOffersByCategoriesNameQuery request, 
+	public async Task<Result<List<NamedList<string, OfferInfoDto>>>> Handle(GetOffersByCategoriesNameQuery request,
 		CancellationToken cancellationToken)
 	{
-		return await _offerRepository.GetOffersByCategoriesNameAsync(request.CategoriesNames, request.Size);
+		var offers = await _offerRepository.GetOffersByCategoriesNameAsync(request.CategoriesNames, request.Size);
+
+		if (offers.IsFailed)
+		{
+			return Result.Fail<List<NamedList<string, OfferInfoDto>>>(offers.Errors);
+		}
+
+		if (_currentUserService.UserId is null)
+		{
+			return offers;
+		}
+
+		var userId = int.Parse(_currentUserService.UserId);
+
+		var favorites = await _offerRepository.GetFavoritesOffersIdsAsync(userId);
+		if (favorites.IsFailed)
+		{
+			return Result.Fail(favorites.Errors);
+		}
+
+		offers.Value.ForEach(l => 
+			l.Data.ForEach(o =>
+			{
+				o.IsFavourite = favorites.Value.Contains(o.Id);
+			}));
+
+		return offers;
 	}
 }

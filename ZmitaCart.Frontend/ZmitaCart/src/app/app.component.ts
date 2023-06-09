dacd4b752eccd246df3c61ac55bfae2a39f7b969
PanJan44 <jasjanek33@gmail.com>
2023-06-09 23:31:44 +0200

    no przecinka zapomnialem

--- ZmitaCart.Frontend/ZmitaCart/src/app/app.component.ts
+++ ZmitaCart.Frontend/ZmitaCart/src/app/app.component.ts
@@ -1,37 +1,37 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
+import {MatIconRegistry} from '@angular/material/icon';
+import {DomSanitizer} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
 
 @Component({
-    selector: 'pp-app-root',
-    standalone: true,
-    imports: [CommonModule, RouterModule],
-    templateUrl: './app.component.html',
+  selector: 'pp-app-root',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
-    constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-        customIcons.forEach(([iconName, icon]) => {
-            iconRegistry.addSvgIcon(
-                iconName,
-                sanitizer.bypassSecurityTrustResourceUrl(`http://localhost:4200/assets/images/${icon}`),
-            );
-        });
-    }
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    customIcons.forEach(([iconName, icon]) => {
+      iconRegistry.addSvgIcon(
+        iconName,
+        sanitizer.bypassSecurityTrustResourceUrl(`http://localhost:4200/assets/images/${icon}`),
+      );
+    });
+  }
 }
 
 const customIcons: [string, string][] = [
-    ["allegro", "allegro.svg"],
-    ["add", "add.svg"],
-    ["arrow", "arrow.svg"],
-    ["chat", "chat.svg"],
-    ["heart", "heart.svg"],
-    ["heart-filled", "heart-filled.svg"],
-    ["user", "user.svg"],
-    ["menu", "menu.svg"],
-    ["find", "find.svg"],
-  ["my-camera", "camera-add.svg"]
-    ["logout", "logout.svg"],
-    ["user-basic", "user-basic.svg"],
+  ["allegro", "allegro.svg"],
+  ["add", "add.svg"],
+  ["arrow", "arrow.svg"],
+  ["chat", "chat.svg"],
+  ["heart", "heart.svg"],
+  ["heart-filled", "heart-filled.svg"],
+  ["user", "user.svg"],
+  ["menu", "menu.svg"],
+  ["find", "find.svg"],
+  ["my-camera", "camera-add.svg"],
+  ["logout", "logout.svg"],
+  ["user-basic", "user-basic.svg"],
 ];
